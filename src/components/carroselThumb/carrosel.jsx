dfef92e6ac4848c7f35158f20a12b1ef2504e49a
Cristiano Moteram <cristiano_moteram@hotmail.com>
2020-08-17 00:22:41 -0300

    atualização no style na parte do form

--- src/components/carroselThumb/carrosel.jsx
+++ src/components/carroselThumb/carrosel.jsx
@@ -10,16 +10,16 @@ return(
                 <Carousel autoPlay interval="2000" transitionTime="1000">
                     <div>
                         <img src={T_11} />
-                        <p className="legend">My Classic Still 1</p>
+                      
                     </div>
                     <div>
                     <img src={T_11} />
-                        <p className="legend">My Classic Still 2</p>
                     </div>
+                       
                     <div>
                     <img src={T_11} />
-                        <p className="legend">My Classic Still 3</p>
                     </div>
+                        
                 </Carousel>
             </div>
 
